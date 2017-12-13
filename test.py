a6f79d2ebda9dde2b88b300b77945a02773d2029
ItsMeMaho <holzinger@cl.uni-heidelberg.de>
2017-12-14 00:53:41 +0100

    final version

--- test.py
+++ test.py
@@ -2,6 +2,8 @@ from cort.core import corpora
 from cort.analysis import error_extractors
 from cort.analysis import spanning_tree_algorithms
 from cort.analysis import plotting
+from collections import Counter
+import pprint
 
 reference = corpora.Corpus.from_file(
     "reference", open("wsj_gold_for_cort.conll")
@@ -29,14 +31,52 @@ errors_by_type = errors.categorize(
 )
 
 # errors_by_type.visualize("pair")
+# common noun errors
+nom_rec_errs = errors_by_type["pair"]["recall_errors"]["all"]['NOM']
+all_heads = [
+    " ".join(err[0].attributes["head"]).lower() for err in nom_rec_errs]
+most_common = Counter(all_heads).most_common(100)
+with open('head_statistics_for_common_noun_errors_errors.txt', 'w') as f:
+    pprint.pprint(most_common, stream=f)
+# proper name
+nam_rec_errs = errors_by_type["pair"]["recall_errors"]["all"]['NAM']
+all_heads = [
+    " ".join(err[0].attributes["head"]).lower() for err in nam_rec_errs]
+most_common = Counter(all_heads).most_common(100)
+with open('head_statistics_for_proper_name_errors_errors.txt', 'w') as f:
+    pprint.pprint(most_common, stream=f)
+# pronoun
+pro_rec_errs = errors_by_type["pair"]["recall_errors"]["all"]['PRO']
+all_heads = [
+    " ".join(err[0].attributes["head"]).lower() for err in pro_rec_errs]
+most_common = Counter(all_heads).most_common(100)
+with open('head_statistics_for_pronoun_errors_errors.txt', 'w') as f:
+    pprint.pprint(most_common, stream=f)
+# demonstrative pronoun
+dem_rec_errs = errors_by_type["pair"]["recall_errors"]["all"]['DEM']
+all_heads = [
+    " ".join(err[0].attributes["head"]).lower() for err in dem_rec_errs]
+most_common = Counter(all_heads).most_common(100)
+with open(
+    'head_statistics_for_demonstrative_pronoun_errors_errors.txt', 'w'
+        ) as f:
+    pprint.pprint(most_common, stream=f)
+# verb
+vrb_rec_errs = errors_by_type["pair"]["recall_errors"]["all"]['VRB']
+all_heads = [
+    " ".join(err[0].attributes["head"]).lower() for err in vrb_rec_errs]
+most_common = Counter(all_heads).most_common(100)
+with open('head_statistics_for_verb_errors_errors.txt', 'w') as f:
+    pprint.pprint(most_common, stream=f)
+
 
 pair_errs = errors_by_type["pair"]["recall_errors"]["all"]
-# pairs_errs = pron_anaphor_errors['pair']['recall_errors']['all']
 plotting.plot(
     [("pair_recall", [(cat, len(errs)) for cat, errs in pair_errs.items()])],
     "Recall Errors",
     "Type of anaphor",
     "Number of Errors")
+plotting.pyplot.savefig('all_error_recall.png')
 
 pair_prec = errors_by_type["pair"]["precision_errors"]["all"]
 plotting.plot(
@@ -45,10 +85,27 @@ plotting.plot(
     "Precision Errors",
     "Type of anaphor",
     "Number of Errors")
+plotting.pyplot.savefig('all_error_precision.png')
 
-pair_dec = errors_by_type["pair"]["decisions"]["all"]
+all_gold = set()
+for doc in reference:
+    for mention in doc.annotated_mentions:
+        all_gold.add(mention)
+
+
+def is_anaphor_gold(mention):
+    if mention in all_gold:
+        return "is_gold"
+    else:
+        return "is_not_gold"
+
+
+is_ana_gold = errors_by_type.categorize(
+    lambda err: is_anaphor_gold(err[0]))["pair"]["recall_errors"]["all"]
 plotting.plot(
-    [("pair_decisions", [(cat, len(errs)) for cat, errs in pair_dec.items()])],
-    "Decisions Errors",
+    [("Status of Anaphor",
+        [(cat, len(errs)) for cat, errs in is_ana_gold.items()])],
+    "Recall Errors",
     "Type of anaphor",
     "Number of Errors")
+plotting.pyplot.savefig('anaphor_status_recall.png')
